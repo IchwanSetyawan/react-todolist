1ffec312983217f472944724e7fc8915676bad87
Ichwan Setyawan <43878679+IchwanSetyawan@users.noreply.github.com>
2020-02-16 11:05:25 +0700

    membuat pages dan component todo

---
+++ src/components/Todos.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Todo from "./Todo";
+
+const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar Props di React"
+    },
+    {
+      text: "Belajar React Hooks"
+    },
+    {
+      text: "Belajar State di React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar Props di React"
+    },
+    {
+      text: "Belajar React Hooks"
+    },
+    {
+      text: "Belajar State di React"
+    }
+  ];
+
+  return (
+    <section className="todos">
+      {todos.map(todo => {
+        return <Todo text={todo.text} />;
+      })}
+    </section>
+  );
+};
+
+export default Todos;
